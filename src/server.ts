3539daad59198a4063f6f445d9f925cd763977fb
Daniel Pinheiro do Nascimento <daniel.pin@outlook.com>
2021-02-24 23:04:46 -0300

    inicio

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { response } from 'express';
+
+const app = express();
+
+/**
+ * GET => BUSCA
+ * POST => SALVAR
+ * PUT => ALTERAR 
+ * DELETE => DELETAR
+ * PATCH => ALTERAÇÃO ESPECÍFICA
+ */
+
+ // http://localhost:3333/users
+app.get("/", (request, response)=>{
+    return response.json({message: "Hello World - NLW04"})
+});
+
+// 1 param => Rota(recurso API)
+// 2 param => request,response
+
+app.post("/", (request, response)=>{
+    return response.json({message: "Os dados foram salvos com sucesso"})
+});
+
+app.listen(3333, () => console.log("Server is running"));
+
