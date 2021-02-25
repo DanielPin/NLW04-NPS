6ad9ffa87a22a252d93fc7c87f81a635b825dbe2
Daniel Pinheiro do Nascimento <daniel.pin@outlook.com>
2021-02-25 00:52:02 -0300

    criando entidade

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import 'reflect-metadata';
 import express, { response } from 'express';
 import "./database";
+import { router } from '../routes';
+
 const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => console.log("Server is running"));
+
+
+
 
 
 
@@ -14,16 +24,13 @@ const app = express();
  */
 
  // http://localhost:3333/users
-app.get("/", (request, response)=>{
-    return response.json({message: "Hello World - NLW04"})
-});
+//app.get("/", (request, response)=>{
+//    return response.json({message: "Hello World - NLW04"})
+//});
 
 // 1 param => Rota(recurso API)
 // 2 param => request,response
 
-app.post("/", (request, response)=>{
-    return response.json({message: "Os dados foram salvos com sucesso"})
-});
-
-app.listen(3333, () => console.log("Server is running"));
-
+//app.post("/", (request, response)=>{
+//   return response.json({message: "Os dados foram salvos com sucesso"})
+//});
