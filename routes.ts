de0dcfa0f4fe69edb96b64bc7211faaf49fc750c
Daniel Pinheiro do Nascimento <daniel.pin@outlook.com>
2021-02-27 00:03:36 -0300

    Adicionado Surveys

--- routes.ts
+++ routes.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import { UserController } from './src/controllers/UserController';
+import { SurveysController } from './src/controllers/SurveysController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
 router.post("/users", userController.create);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export {router};
