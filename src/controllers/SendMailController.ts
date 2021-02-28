5ec0d38825fd1307b41a7c37981198385931ff13
Daniel Pinheiro do Nascimento <daniel.pin@outlook.com>
2021-02-28 00:57:50 -0300

    Final

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -5,6 +5,7 @@ import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
 import {resolve} from 'path';
+import { AppError } from "../errors/AppError";
 
 class SendMailController{
     async execute(request: Request, response: Response){
@@ -17,17 +18,13 @@ class SendMailController{
         const userAlreadyExists = await usersRepository.findOne({ email });
 
         if(!userAlreadyExists){
-            return response.status(400).json({
-                error: "User does not exists"
-            });
+            throw new AppError("User does not exists");          
         };
 
         const surveyAlreadyExists = await surveyRepository.findOne({id: survey_id})
 
         if(!surveyAlreadyExists){
-            return response.status(400).json({
-                error: "Survey does not exists"
-            });
+            throw new AppError("Survey does not exists");              
         };       
 
       
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup'
+import { AppError } from '../errors/AppError';
 
 class UserController{
 
     async create(request: Request,response: Response){
         const {name, email} = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatorio"),
+            email: yup.string().email().required("Email é obrigatorio")
+        });
+        /*
+        if(!(await schema.isValid(request.body))){
+            return response.status(400).json({error: "Validation Failed"});
+        }
+        */
+       try{
+           await schema.validate(request.body, {abortEarly: false});
+       }catch(err){
+            throw new AppError(err);      
+       }
 
         const usersRepository =  getCustomRepository(UsersRepository);
 
@@ -14,11 +31,7 @@ class UserController{
         });
 
         if(userAlreadyExists){
-            return response.status(400).json({
-                error: "Usuario ja existe"
-            });
-
-            
+            throw new AppError("Usuario ja existe");                 
         }
 
         const user = usersRepository.create({
